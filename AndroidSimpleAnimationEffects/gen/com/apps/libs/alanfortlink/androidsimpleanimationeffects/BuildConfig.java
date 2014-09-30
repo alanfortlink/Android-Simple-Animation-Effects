9c9e15122a3efef3f908981ff019cc8645fd7c41
alan <alan@alan-A14CR6A.(none)>
2014-09-30 15:36:54 -0300

    Init of my project, :D

---
+++ AndroidSimpleAnimationEffects/gen/com/apps/libs/alanfortlink/androidsimpleanimationeffects/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.apps.libs.alanfortlink.androidsimpleanimationeffects;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
